ed2726c7e7257646a1df1788f6eafa4dd8a780e9
introtoxicXXl <minhajulabedin648@gmail.com>
2023-10-31 22:42:07 +0600

    Cart component layout design done

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
 
 Cart.propTypes = {
-    
+
 };
 
 function Cart() {
     return (
-        <div className='w-1/4'>
-            kjsfvjisd
+        <div className='w-1/4 ml-4'>
+            <div className='bg-white p-6 rounded-lg'>
+                <div>
+                    <h2 className='text-lg font-bold text-sky-500'>Credit Hour Remaining 7 hr</h2>
+                    <hr className='my-2' />
+                    <h2 className='text-xl font-bold mb-4'>Course Name</h2>
+                    <ul role="list" className='list-decimal mb-3'>
+                       
+                    </ul>
+                    <hr className='my-2' />
+                    <h2 className='text-base font-semibold'>Total Credit Hour : 13</h2>
+                    <hr className='my-2' />
+                    <h2 className='text-base font-semibold'>Total Price : 48000 USD</h2>
+                </div>
+            </div>
         </div>
     );
 }
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,8 @@
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-3xl font-bold text-center my-4'>Course Registration</h1>
+        <div className="py-4">
+            <h1 className='text-3xl font-bold text-center'>Course Registration</h1>
         </div>
     );
 };
